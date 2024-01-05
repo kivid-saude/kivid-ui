7f987cd9948f6daf137fbd4fc033f055114cb4fd
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2024-01-04 23:52:04 -0300

    Implementando forwardRef

--- src/components/KvInput/KvInput.tsx
+++ src/components/KvInput/KvInput.tsx
@@ -1,8 +1,12 @@
+import React from "react";
+
 type TInput = React.DetailedHTMLProps<
   React.InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
 >;
 
-export const KvInput = (props: TInput) => {
-  return <input className="kv-input" type="text" {...props} />;
-};
+export const KvInput = React.forwardRef<HTMLInputElement, TInput>(
+  (props, ref) => {
+    return <input className="kv-input" type="text" {...props} ref={ref} />;
+  }
+);
