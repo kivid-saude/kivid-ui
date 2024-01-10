bd42ee751bd594ae4c70577accda587b1ae9440c
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2024-01-10 00:37:28 -0300

    Refatorando

--- src/components/KvButtons/KvButtons.tsx
+++ src/components/KvButtons/KvButtons.tsx
@@ -3,11 +3,14 @@ import "./kv-buttons.css";
 
 type TKvButtons = {
   column?: boolean;
-} & PropsWithChildren;
+} & React.DetailedHTMLProps<
+  React.MenuHTMLAttributes<HTMLElement>,
+  HTMLElement
+> &
+  PropsWithChildren;
 
 export const KvButtons = (props: TKvButtons) => {
   const classes = ["kv-buttons"];
-
   if (props.column) classes.push("kv-buttons--column");
 
   return <menu className={classes.join(" ")}>{props.children}</menu>;
