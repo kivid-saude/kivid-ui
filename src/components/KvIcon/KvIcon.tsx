b4f710876682e2b6896f45d743ac5bd318a9a405
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2024-01-30 18:22:08 -0300

    Atualizado

--- src/components/KvIcon/KvIcon.tsx
+++ src/components/KvIcon/KvIcon.tsx
@@ -1,14 +1,15 @@
-import icons from "./kivid-icons.svg";
 import "./kv-icon.css";
 
 type TKvIcon = {
   icon: string;
 } & React.SVGProps<SVGSVGElement>;
 
+const iconsPath = "/kivid-icons/spritesheet.svg";
+
 export const KvIcon = ({ icon, className = "", ...props }: TKvIcon) => {
   return (
     <svg className={`kv-icon ${className}`} {...props}>
-      <use href={`${icons}#${icon}`} />
+      <use href={`${iconsPath}#${icon}`} />
     </svg>
   );
 };
