dbe344115be258946ae421a6a2fa32ba82af24ad
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2024-02-23 17:10:15 -0300

    Atualizado

--- src/components/KvTitle/KvTitle.tsx
+++ src/components/KvTitle/KvTitle.tsx
@@ -47,7 +47,7 @@ export const KvTitle = ({
       <header className={`kv-header ${colorClass} ${sizeClass}`}>
         {!!icon && (
           <div className="kv-title-icon">
-            <KvIcon icon={icon} />
+            <KvIcon className="kv-color-white" icon={icon} />
           </div>
         )}
 
