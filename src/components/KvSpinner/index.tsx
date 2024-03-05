308bf5b51d6afe0bca9afffe381a504eb456ad85
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2024-03-05 16:10:07 -0300

    Exportando como default

--- src/components/KvSpinner/index.tsx
+++ src/components/KvSpinner/index.tsx
@@ -5,7 +5,7 @@ import { Player } from "@lottiefiles/react-lottie-player";
 import loadingPurple from "./loading-button-purple.json";
 import loadingWhite from "./loading-button-white.json";
 
-const KvSpinner = ({
+export const KvSpinner = ({
   color,
 }: {
   color?: "success" | "danger" | "tertiary" | "warning" | "light" | "dark";
