47a2e2128450478c074aa2b2b24252557d88fcc4
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2024-04-02 17:19:27 -0300

    feat: Implementando Storybook

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "./src/styles/variables.scss";
+        `,
+      },
+    },
+  },
+});
