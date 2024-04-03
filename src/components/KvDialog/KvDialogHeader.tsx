dc05525a4ab9784eb3aabc409082536b53baf3f5
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2024-04-03 18:51:28 -0300

    refatorando

--- src/components/KvDialog/KvDialogHeader.tsx
+++ src/components/KvDialog/KvDialogHeader.tsx
@@ -1,6 +1,6 @@
 import styles from "./styles.module.css";
 
 export const KvDialogHeader = ({ children }: React.PropsWithChildren) => {
-  if (!children) return null;
+  if (!children) return <div></div>;
   return <header className={styles.dialog__header}>{children}</header>;
 };
