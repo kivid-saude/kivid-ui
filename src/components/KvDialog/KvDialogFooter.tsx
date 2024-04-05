0831ca54b62308b2044779cc0171e8b75a453dba
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2024-04-05 14:58:26 -0300

    fix: Retornando div vazia caso não tenha children

--- src/components/KvDialog/KvDialogFooter.tsx
+++ src/components/KvDialog/KvDialogFooter.tsx
@@ -1,5 +1,6 @@
 import styles from "./styles.module.css";
 
 export const KvDialogFooter = ({ children }: React.PropsWithChildren) => {
+  if (!children) return <div></div>;
   return <footer className={styles.dialog__footer}>{children}</footer>;
 };
