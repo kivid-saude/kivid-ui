424e169c5b00644f36143aa4048fc0a94de07799
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2024-04-08 19:04:14 -0300

    Removendo import global

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `
-          @import "./src/styles/variables.scss";
-        `,
-      },
-    },
-  },
+  // css: {
+  //   preprocessorOptions: {
+  //     scss: {
+  //       additionalData: `
+  //         @import "./src/styles/variables.scss";
+  //       `,
+  //     },
+  //   },
+  // },
 });
