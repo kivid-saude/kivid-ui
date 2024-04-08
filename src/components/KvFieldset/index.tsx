09301c7ff5df28f3addaf87846c64a660768554c
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2024-04-08 18:46:05 -0300

    fix: Corrigindo elementos com label

--- src/components/KvFieldset/index.tsx
+++ src/components/KvFieldset/index.tsx
@@ -1,14 +1,19 @@
-import { PropsWithChildren } from "react";
+import React, { PropsWithChildren } from "react";
+import { KvLabel } from "../KvLabel";
 
 type TFieldset = PropsWithChildren;
 
 export const KvFieldset = ({ children }: TFieldset) => {
+  const classes: string[] = [];
+
+  React.Children.forEach(children, (child) => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const bool = (child as any).type === KvLabel;
+    bool && classes.push("kv-fieldset--has-label");
+  });
+
   return (
-    <fieldset
-      className={`kv-fieldset ${
-        Array.isArray(children) ? "kv-fieldset--has-label" : ""
-      }`}
-    >
+    <fieldset className={`kv-fieldset ${classes.join(" ")}`}>
       {children}
     </fieldset>
   );
