a0513f20992b37b4f1dfe2ce2effcca5481ec6df
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2024-04-08 17:09:50 -0300

    Implementando KvAction Storybook

--- src/components/KvAction/index.tsx
+++ src/components/KvAction/index.tsx
@@ -1,12 +1,35 @@
 import { PropsWithChildren } from "react";
-import "./kv-action.css";
+import { KeyColors, MapColors } from "../../types/styles";
+import { KvIcon } from "../KvIcon";
+import styles from "./style.module.scss";
 
-type TKvAction = React.HTMLAttributes<HTMLDivElement> & PropsWithChildren;
+type TKvAction = {
+  label: string;
+  color?: KeyColors;
+  fill?: "solid" | "outline";
+  icon?: string;
+  iconColor?: KeyColors;
+} & React.HTMLAttributes<HTMLDivElement> &
+  PropsWithChildren;
 
-export const KvAction = ({ children, className = "", ...props }: TKvAction) => {
+export const KvAction = ({
+  label,
+  color = "light",
+  fill = "outline",
+  icon,
+  iconColor,
+  ...props
+}: TKvAction) => {
+  const classes = [
+    styles["kv-action"],
+    styles[`kv-action--${fill}`],
+    MapColors[color],
+  ].join(" ");
+
   return (
-    <div className={`kv-action ${className}`} {...props}>
-      {children}
+    <div className={classes} {...props}>
+      {!!icon && <KvIcon color={iconColor} icon={icon} />}
+      {label}
     </div>
   );
 };
