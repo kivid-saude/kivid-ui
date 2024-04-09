1a5a85d9d568050d0a4ef880533961abfa5f71e7
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2024-04-08 22:55:06 -0300

    fix: KvFieldset - corrigindo tipagem

--- src/components/KvFieldset/index.tsx
+++ src/components/KvFieldset/index.tsx
@@ -7,8 +7,7 @@ export const KvFieldset = ({ children }: TFieldset) => {
   const classes: string[] = [];
 
   React.Children.forEach(children, (child) => {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const bool = child && (child as any).type === KvLabel;
+    const bool = child && (child as { type: unknown }).type === KvLabel;
     bool && classes.push("kv-fieldset--has-label");
   });
 
