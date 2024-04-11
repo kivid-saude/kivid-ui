9d708481c2579f367caa618d35346a5d3ed53c38
Matheus <matheus.araujo@kivid.com.br>
2024-04-11 09:31:25 -0300

    feat: create radio atomic component

---
+++ src/components/KvRadioInput/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './style.module.scss'
+
+type TInput = React.InputHTMLAttributes<HTMLInputElement>
+
+export const KvRadioInput = React.forwardRef<HTMLInputElement, TInput>(
+  ({ className = '', ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        type='radio'
+        id={props.id}
+        className={`${styles['kv-input-radio']} ${className}`}
+        {...props}
+      />
+    )
+  }
+)
+
+KvRadioInput.displayName = 'KvRadio'
