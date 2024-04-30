9dfaa22f0e28d74a4a1f5ae6be56b2a818a90d2a
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2024-04-29 21:05:55 -0300

    fix: Smask rerender

--- src/components/Smask/index.tsx
+++ src/components/Smask/index.tsx
@@ -2,25 +2,50 @@ import React, { useEffect, useState } from "react";
 import * as smask from "smask";
 
 type TSmask = {
-  children: React.ReactElement;
   mask: [string, ...string[]];
   value?: string;
-};
+} & React.PropsWithChildren;
 
-export const Smask = ({ children, mask, value = "" }: TSmask) => {
-  const [newValue, setNewValue] = useState(value);
+export const Smask = ({ children, mask, ...props }: TSmask) => {
+  const [newValue, setNewValue] = useState(props.value);
 
-  const minLength = mask.at(0)!.length;
-  const maxLength = mask.at(-1)!.length;
+  const minLength = mask?.at?.(0)?.length;
+  const maxLength = mask?.at?.(-1)?.length;
+
+  const onInput = (event: React.FormEvent<HTMLInputElement>) => {
+    const { value } = event.currentTarget;
+
+    if (!value) {
+      setNewValue(value);
+      return;
+    }
+
+    try {
+      const maskedValue = smask.mask(value, mask);
+      setNewValue(maskedValue);
+    } catch {
+      setNewValue(value);
+    }
+  };
 
   useEffect(() => {
-    if (value === newValue) return;
-    const maskedValue = value ? smask.mask(value, mask) : "";
+    if (!props.value) return;
+    if (props.value === newValue) return;
+    const maskedValue = smask.mask(props.value, mask);
     setNewValue(maskedValue);
-  }, [mask, newValue, value]);
+  }, [mask, newValue, props.value]);
 
-  return React.cloneElement(children, {
+  if (props.value) {
+    return React.cloneElement(children as React.ReactElement, {
+      value: newValue,
+      minLength,
+      maxLength,
+    });
+  }
+
+  return React.cloneElement(children as React.ReactElement, {
     value: newValue,
+    onInput,
     minLength,
     maxLength,
   });
