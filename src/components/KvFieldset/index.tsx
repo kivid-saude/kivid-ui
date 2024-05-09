157e1cd8fa57cae94ab64e8c537286a3bcd2cab3
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2024-05-09 13:21:07 -0300

    Refactored

--- src/components/KvFieldset/index.tsx
+++ src/components/KvFieldset/index.tsx
@@ -1,19 +1,16 @@
 import React, { PropsWithChildren } from "react";
 import { KvLabel } from "../KvLabel";
+import styles from "./styles.module.scss";
 
 type TFieldset = PropsWithChildren;
 
 export const KvFieldset = ({ children }: TFieldset) => {
-  const classes: string[] = [];
+  const classes = [styles["kv-fieldset"]];
 
   React.Children.forEach(children, (child) => {
     const bool = child && (child as { type: unknown }).type === KvLabel;
     bool && classes.push("kv-fieldset--has-label");
   });
 
-  return (
-    <fieldset className={`kv-fieldset ${classes.join(" ")}`}>
-      {children}
-    </fieldset>
-  );
+  return <fieldset className={classes.join(" ")}>{children}</fieldset>;
 };
