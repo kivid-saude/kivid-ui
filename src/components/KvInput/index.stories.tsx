a8cfd5ec41eacef3169169434c60ccb38ed431c4
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2024-05-09 13:22:12 -0300

    KvInput: Refactor and new styles

---
+++ src/components/KvInput/index.stories.tsx
@@ -0,0 +1,106 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { KvInput } from ".";
+import { KvFieldset } from "../KvFieldset";
+import { KvLabel } from "../KvLabel";
+
+const meta = {
+  title: "Kivid/KvInput",
+  component: KvInput,
+  parameters: {
+    // layout: "centered",
+  },
+  tags: ["autodocs"],
+  argTypes: {},
+} satisfies Meta<typeof KvInput>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => (
+    <KvFieldset>
+      <KvInput placeholder="Digite aqui" {...args} />
+    </KvFieldset>
+  ),
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+  },
+  render: (args) => (
+    <KvFieldset>
+      <KvInput placeholder="Digite aqui" {...args} />
+    </KvFieldset>
+  ),
+};
+
+export const Valid: Story = {
+  args: {
+    status: "valid",
+  },
+  render: (args) => (
+    <KvFieldset>
+      <KvInput placeholder="Digite aqui" {...args} />
+    </KvFieldset>
+  ),
+};
+
+export const Invalid: Story = {
+  args: {
+    status: "invalid",
+    errorMessage: "Campo obrigatório",
+  },
+  render: (args) => (
+    <KvFieldset>
+      <KvInput placeholder="Digite aqui" {...args} />
+    </KvFieldset>
+  ),
+};
+
+export const WithLabel: Story = {
+  render: (args) => (
+    <KvFieldset>
+      <KvLabel>Label</KvLabel>
+      <KvInput placeholder="Digite aqui" {...args} />
+    </KvFieldset>
+  ),
+};
+
+export const DisabledWithLabel: Story = {
+  args: {
+    disabled: true,
+  },
+  render: (args) => (
+    <KvFieldset>
+      <KvLabel>Label</KvLabel>
+      <KvInput placeholder="Digite aqui" {...args} />
+    </KvFieldset>
+  ),
+};
+
+export const ValidWithLabel: Story = {
+  args: {
+    status: "valid",
+  },
+  render: (args) => (
+    <KvFieldset>
+      <KvLabel>Label</KvLabel>
+      <KvInput placeholder="Digite aqui" {...args} />
+    </KvFieldset>
+  ),
+};
+
+export const InvalidWithLabel: Story = {
+  args: {
+    status: "invalid",
+    errorMessage: "Campo obrigatório",
+  },
+  render: (args) => (
+    <KvFieldset>
+      <KvLabel>Label</KvLabel>
+      <KvInput placeholder="Digite aqui" {...args} />
+    </KvFieldset>
+  ),
+};
--- src/components/KvInput/index.tsx
+++ src/components/KvInput/index.tsx
@@ -1,21 +1,63 @@
 import React from "react";
-import "./kv-input.css";
+import { KvSeal } from "../KvSeal";
+import { KvTooltip } from "../KvTooltip";
+import styles from "./styles.module.scss";
 
-type TKvInput = {
+export type TKvInputStatus = "idle" | "invalid" | "valid";
+
+export type TKvInput = {
   errorMessage?: string;
   rounded?: boolean;
+  status?: TKvInputStatus;
 } & React.InputHTMLAttributes<HTMLInputElement>;
 
 const KvInput = React.forwardRef<HTMLInputElement, TKvInput>(
-  ({ className = "", errorMessage, rounded = true, ...props }, ref) => {
-    const classes = ["kv-input"];
-    if (rounded) classes.push("kv-input--rounded");
-    if (errorMessage) classes.push("kv-input--error");
+  (
+    {
+      className = "",
+      errorMessage = "",
+      rounded = true,
+      status = "idle",
+      ...props
+    },
+    ref,
+  ) => {
+    const classes = [styles["kv-input"]];
+    if (rounded) classes.push(styles["kv-input--rounded"]);
+    if (status === "invalid") classes.push(styles["kv-input--invalid"]);
     if (className) classes.push(className);
     const computedClasses = classes.join(" ");
 
     return (
-      <input className={computedClasses} type="text" ref={ref} {...props} />
+      <>
+        {status === "invalid" && (
+          <KvSeal
+            className="slot slot--right"
+            icon="close"
+            color="danger"
+            size="medium"
+            disabled={props.disabled}
+          />
+        )}
+
+        {status === "valid" && (
+          <KvSeal
+            className="slot slot--right"
+            icon="check"
+            color="success"
+            size="medium"
+            disabled={props.disabled}
+          />
+        )}
+
+        <KvTooltip
+          content={errorMessage}
+          open={!!errorMessage}
+          status={status === "invalid" ? status : undefined}
+        >
+          <input className={computedClasses} type="text" ref={ref} {...props} />
+        </KvTooltip>
+      </>
     );
   },
 );
