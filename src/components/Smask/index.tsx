29dcb4c00588e41465e0947eb982d732fe69783f
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2024-06-21 10:58:48 -0300

    feat(Smask): 6875539122

--- src/components/Smask/index.tsx
+++ src/components/Smask/index.tsx
@@ -15,49 +15,55 @@ export type TSmask = TSmaskUncontrolled | TSmaskControlled;
 export const Smask = (props: TSmask) => {
   const [newValue, setNewValue] = useState("value" in props ? props.value : "");
 
-  const setMaskedValue = useCallback((value: string) => {
-    let maskedValue: string;
+  const setMaskedValue = useCallback(
+    (value: string) => {
+      let maskedValue: string;
 
-    if (props.mask === "number") {
-      const number = Number(value.replace(/\D/g, ""));
-      maskedValue = smask.numberFormat(number);
-      console.log("maskedValue :", maskedValue);
-    } else if (props.mask === "currency") {
-      const number = Number(value.replace(/\D/g, ""));
-      maskedValue = smask.currencyFormat(number / 100);
-    } else {
-      const maskLettersLength = props.mask.at(-1)!.replace(/\W/g, "").length;
-      const unmaskedValue = value
-        .replace(/\W/g, "")
-        .slice(0, maskLettersLength);
-      maskedValue = smask.mask(unmaskedValue, props.mask);
-    }
+      if (props.mask === "number") {
+        const number = Number(value.replace(/\D/g, ""));
+        maskedValue = smask.numberFormat(number);
+        console.log("maskedValue :", maskedValue);
+      } else if (props.mask === "currency") {
+        const number = Number(value.replace(/\D/g, ""));
+        maskedValue = smask.currencyFormat(number / 100, "pt-br", "BRL");
+      } else {
+        const maskLettersLength = props.mask.at(-1)!.replace(/\W/g, "").length;
+        const unmaskedValue = value
+          .replace(/\W/g, "")
+          .slice(0, maskLettersLength);
+        maskedValue = smask.mask(unmaskedValue, props.mask);
+      }
 
-    setNewValue(maskedValue);
-  }, []);
+      setNewValue(maskedValue);
+    },
+    [props.mask],
+  );
 
-  const onChange = useCallback((event: React.FormEvent<HTMLInputElement>) => {
-    const { value } = event.currentTarget;
+  const onChange = useCallback(
+    (event: React.FormEvent<HTMLInputElement>) => {
+      const { value } = event.currentTarget;
 
-    if (!value) {
-      setNewValue(value);
-      return;
-    }
+      if (!value) {
+        setNewValue(value);
+        return;
+      }
 
-    try {
-      setMaskedValue(value);
-      props.children?.props?.onChange?.(event);
-    } catch {
-      setNewValue(value);
-    }
-  }, []);
+      try {
+        setMaskedValue(value);
+        props.children?.props?.onChange?.(event);
+      } catch {
+        setNewValue(value);
+      }
+    },
+    [props.children?.props],
+  );
 
   useEffect(() => {
     if (!("value" in props)) return;
     if (typeof props.value !== "string") return;
 
     setMaskedValue(props.value);
-  }, [props]);
+  }, [props, setMaskedValue]);
 
   if (props.mask === "number" || props.mask === "currency") {
     return React.cloneElement(props.children!, {
