d582df15ece439c2752d5da53e1652a519437414
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2025-02-19 14:06:19 -0300

    feat: Add CheckButton

--- src/components/KvCheckButton/index.tsx
+++ src/components/KvCheckButton/index.tsx
@@ -1,34 +1,35 @@
 import React, { PropsWithChildren, useState } from "react";
 import { KvButton, TKvButton } from "../KvButton";
-import { KvCheckbox } from "../KvCheckbox";
 
 type TKvCheckButton = {
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
-  checked?: boolean;
 } & Pick<TKvButton, "color" | "size"> &
   PropsWithChildren &
   React.InputHTMLAttributes<HTMLInputElement>;
 
 const KvCheckButton = React.forwardRef<HTMLInputElement, TKvCheckButton>(
-  ({ children, color, size, ...props }, ref) => {
-    const { onChange, checked = false } = props;
-    const [isChecked, setIsChecked] = useState(checked);
+  ({ children, color, size, onChange, ...props }, ref) => {
+    const [isChecked, setIsChecked] = useState(props.checked);
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setIsChecked(!isChecked);
+      onChange?.(event);
+    };
+
     return (
-      <div>
-        <div style={{ display: "none" }}>
-          <KvCheckbox
-            {...props}
-            checked={isChecked}
-            onChange={(event) => onChange?.(event)}
-            ref={ref}
-          />
-        </div>
+      <div className="relative">
+        <input
+          type="checkbox"
+          className="absolute inset-0 z-10 opacity-0 w-full h-full m-0 p-0 cursor-pointer"
+          ref={ref}
+          onChange={handleInputChange}
+          {...props}
+        />
         <KvButton
           type="button"
           size={size ?? "large"}
           color={isChecked ? color ?? "tertiary" : "muted"}
           style={{ minWidth: "unset", padding: "0.75rem" }}
-          onClick={() => setIsChecked(!isChecked)}
         >
           {children}
         </KvButton>
