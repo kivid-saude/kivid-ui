e93706be41451857bb88270413464daceffcbc4b
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2025-02-19 17:36:22 -0300

    fix: remove hasField

--- src/components/KvFieldset/index.tsx
+++ src/components/KvFieldset/index.tsx
@@ -3,18 +3,17 @@ import { KvLabel } from "../KvLabel";
 import styles from "./styles.module.scss";
 
 type TFieldset = {
-  className?: string
-  hasLabel?: boolean
+  className?: string;
 } & PropsWithChildren;
 
-export const KvFieldset = ({ children, className, hasLabel = true }: TFieldset) => {
+export const KvFieldset = ({ children, className }: TFieldset) => {
   const classes = [styles["kv-fieldset"]];
   if (className) classes.push(className);
 
   React.Children.forEach(children, (child) => {
+    console.log("child :", child);
     const bool = child && (child as { type: unknown }).type === KvLabel;
-    bool || hasLabel && classes.push("kv-fieldset--has-label")
-    !hasLabel && classes.push("kv-fieldset--no-label")
+    bool && classes.push("kv-fieldset--has-label");
   });
 
   return <fieldset className={classes.join(" ")}>{children}</fieldset>;
