f5baf61b501fa98149401a8008f1ae94f6e52077
Marcelo Ribeiro <marceloribeiro.dev@gmail.com>
2025-02-25 15:25:37 -0300

    fix: Fix KvMultiSelect

--- src/components/KvMultiSelect/index.tsx
+++ src/components/KvMultiSelect/index.tsx
@@ -21,138 +21,122 @@ export type TKvMultiSelect = {
   onSelectedChange?: (value: string[]) => void;
 };
 
-const KvMultiSelect = React.forwardRef<
-  HTMLDivElement,
-  TKvMultiSelect & React.ComponentProps<"div">
->(
-  ({
-    options,
-    placeholder = "Selecione...",
-    value = [],
-    closeOnOutsideClick = true,
-    onSelectedChange,
-    listProps,
-    ...props
-  }) => {
-    const [selected, setSelected] = useState<Option[]>([]);
-    const [isOpen, setIsOpen] = useState(false);
-    const [search, setSearch] = useState("");
-    const multiSelectRef = useRef<HTMLDivElement | null>(null);
+export const KvMultiSelect = ({
+  options,
+  placeholder = "Selecione...",
+  value = [],
+  closeOnOutsideClick = true,
+  onSelectedChange,
+  listProps,
+  ...props
+}: TKvMultiSelect) => {
+  const [selected, setSelected] = useState<Option[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [search, setSearch] = useState("");
+  const multiSelectRef = useRef<HTMLDivElement>(null);
 
-    useEffect(() => {
-      if (!value?.length || !options?.length) return;
-      const selectedOption = options.filter((option) =>
-        value.includes(option.value),
-      );
-      setSelected((prevSelected) => selectedOption ?? prevSelected);
-    }, [value, options]);
-
-    const filteredOptions = options.filter((option) =>
-      option.label.toLowerCase().includes(search.toLowerCase()),
+  useEffect(() => {
+    if (!value?.length || !options?.length) return;
+    const selectedOption = options.filter((option) =>
+      value.includes(option.value),
     );
+    setSelected((prevSelected) => selectedOption ?? prevSelected);
+  }, [value, options]);
 
-    const handleSelectedChange = (option: Option) => {
-      setSelected((prevSelected) => {
-        const hasSelected = prevSelected?.some(
-          ({ value }) => value === option.value,
-        );
-        const newSelected = hasSelected
-          ? prevSelected?.filter(({ value }) => value !== option.value)
-          : [...(prevSelected ?? []), option];
-        onSelectedChange?.(newSelected.map(({ value }) => value));
-        return newSelected;
-      });
+  const filteredOptions = options.filter((option) =>
+    option.label.toLowerCase().includes(search.toLowerCase()),
+  );
+
+  const handleSelectedChange = (option: Option) => {
+    setSelected((prevSelected) => {
+      const hasSelected = prevSelected?.some(
+        ({ value }) => value === option.value,
+      );
+      const newSelected = hasSelected
+        ? prevSelected?.filter(({ value }) => value !== option.value)
+        : [...(prevSelected ?? []), option];
+      onSelectedChange?.(newSelected.map(({ value }) => value));
+      return newSelected;
+    });
+  };
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        closeOnOutsideClick &&
+        multiSelectRef.current &&
+        !multiSelectRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
     };
 
-    useEffect(() => {
-      const handleClickOutside = (event: MouseEvent) => {
-        if (
-          closeOnOutsideClick &&
-          multiSelectRef.current &&
-          !multiSelectRef.current.contains(event.target as Node)
-        ) {
-          setIsOpen(false);
-        }
+    if (closeOnOutsideClick) {
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
       };
+    }
+  }, [closeOnOutsideClick]);
 
-      if (closeOnOutsideClick) {
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => {
-          document.removeEventListener("mousedown", handleClickOutside);
-        };
-      }
-    }, [closeOnOutsideClick]);
-
-    return (
-      <div
-        className={`kv-multi-select relative`}
-        ref={multiSelectRef}
-        {...props}
-      >
-        <div
-          className="kv-multi-select-value"
-          onClick={() => setIsOpen(!isOpen)}
-        >
-          {selected?.length
-            ? selected
-                .map(({ label }) => label)
-                .sort()
-                .join(", ")
-            : placeholder}
-        </div>
-
-        <KvIcon className="slot slot--right" icon="chevron-down" />
-
-        {!!selected?.length && (
-          <div style={{ position: "absolute", right: "3rem", top: "1.5rem" }}>
-            <KvBadge
-              color="secondary"
-              label={selected?.length.toString() ?? ""}
-            />
-          </div>
-        )}
-
-        {isOpen && (
-          <div className="kv-multi-select-list">
-            <div style={{ padding: "0 1rem" }}>
-              <KvMultiSelectHeader>
-                <KvSearch
-                  placeholder="Digite para buscar"
-                  status={search ? "clean" : "idle"}
-                  value={search}
-                  isSmall
-                  onChange={(e) => setSearch(e.target.value)}
-                  onClean={() => setSearch("")}
-                />
-              </KvMultiSelectHeader>
-            </div>
-
-            <div style={{ padding: "0 0.5rem" }}>
-              <ul {...listProps}>
-                {filteredOptions.map((option) => (
-                  <li
-                    key={option.value}
-                    className="kv-multi-select-item"
-                    onClick={() => handleSelectedChange(option)}
-                  >
-                    <KvCheckbox
-                      id={option.value}
-                      name={option.value}
-                      checked={selected?.some((s) => s.value === option.value)}
-                      label={option.label}
-                      color="secondary"
-                    />
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        )}
+  return (
+    <div className={`kv-multi-select relative`} ref={multiSelectRef} {...props}>
+      <div className="kv-multi-select-value" onClick={() => setIsOpen(!isOpen)}>
+        {selected?.length
+          ? selected
+              .map(({ label }) => label)
+              .sort()
+              .join(", ")
+          : placeholder}
       </div>
-    );
-  },
-);
 
-KvMultiSelect.displayName = "KvMultiSelect";
+      <KvIcon className="slot slot--right" icon="chevron-down" />
 
-export { KvMultiSelect as KvMultiSelect };
+      {!!selected?.length && (
+        <div style={{ position: "absolute", right: "3rem", top: "1.5rem" }}>
+          <KvBadge
+            color="secondary"
+            label={selected?.length.toString() ?? ""}
+          />
+        </div>
+      )}
+
+      {isOpen && (
+        <div className="kv-multi-select-list">
+          <div style={{ padding: "0 1rem" }}>
+            <KvMultiSelectHeader>
+              <KvSearch
+                placeholder="Digite para buscar"
+                status={search ? "clean" : "idle"}
+                value={search}
+                isSmall
+                onChange={(e) => setSearch(e.target.value)}
+                onClean={() => setSearch("")}
+              />
+            </KvMultiSelectHeader>
+          </div>
+
+          <div style={{ padding: "0 0.5rem" }}>
+            <ul {...listProps}>
+              {filteredOptions.map((option) => (
+                <li
+                  key={option.value}
+                  className="kv-multi-select-item"
+                  onClick={() => handleSelectedChange(option)}
+                >
+                  <KvCheckbox
+                    id={option.value}
+                    name={option.value}
+                    checked={selected?.some((s) => s.value === option.value)}
+                    label={option.label}
+                    color="secondary"
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
